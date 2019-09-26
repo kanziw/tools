5f0b39a0340b7554ba12097e597216cb6b6a2325
kanziw <kanziwoong@gmail.com>
2019-09-27 02:21:59 +0900

    Enhance CD process without actions

--- .github/actions/gcloud_sdk/entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# https://help.github.com/en/articles/creating-a-docker-container-action
-
-gcloud auth activate-service-account --key-file=/tools-kanziw-dev-deploy-keys.json
-gcloud app deploy --project=tools-kanziw-dev
--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 # https://help.github.com/en/articles/virtual-environments-for-github-actions#limits-for-secrets
 
+# Decrypt the file
+mkdir $HOME/secrets
 # --batch to prevent interactive command --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" \
-  --output .github/actions/gcloud_sdk/$1 \
-  .github/scripts/$1.gpg
+  --output $HOME/secrets/$1 .github/scripts/$1.gpg
