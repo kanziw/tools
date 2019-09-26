0ac0a470d0768f224a891d15e9bc0e4a19045432
kanziw <kanziwoong@gmail.com>
2019-09-26 22:19:03 +0900

    Add deploy scripts

--- src/index.js
+++ src/index.js
@@ -5,8 +5,15 @@ const { PORT } = require('./config')
 const app = Express()
 app.use(morgan('tiny'))
 
+const e = process.env
+const GAE_CONFIGURATION = Object
+    .keys(e)
+    .filter(key => key.startsWith('GAE_'))
+    .map(key => `> ${key}: ${e[key]}`)
+    .join('<br />')
+
 app.get('/', (_, res) => {
-  res.send(`Hello? I'm tools`)
+  res.send(`Hello? I'm tools on version<br />${GAE_CONFIGURATION}`)
 })
 
 app.listen(PORT, () => {
