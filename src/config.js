650bed50cb12671beb1ae6fa85930baddd1f2fee
kanziw <kanziwoong@gmail.com>
2019-09-28 00:44:04 +0900

    Serve react application via express when NODE_ENV=production

--- src/config.js
+++ src/config.js
@@ -4,7 +4,9 @@ const s = (prop, defaultValue) => e[ prop ] || defaultValue
 const n = (prop, defaultValue) => parseInt(s(prop, defaultValue), 10)
 
 const PORT = n('PORT', 8080)
+const isProd = s('NODE_ENV') === 'production'
 
 module.exports = {
   PORT,
+  isProd,
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
+const { join, resolve } = require('path')
 const Express = require('express')
 const morgan = require('morgan')
-const { PORT } = require('./config')
+const { PORT, isProd } = require('./config')
 
 const app = Express()
 app.use(morgan('tiny'))
 
-app.get('/', (_, res) => {
-  res.send(`Hello? I'm tools with version ${process.env.GAE_VERSION}`)
+app.get('/hi', (_, res) => {
+  res.send(`hi? I'm tools with version ${process.env.GAE_VERSION}`)
 })
+
+if (isProd) {
+  const outputPath = join(__dirname, '../public')
+  app.use('/', Express.static(outputPath))
+  app.get('*', (req, res) => res.sendFile(resolve(outputPath, 'index.html')))
+}
 
 app.listen(PORT, () => {
   console.log(`tools is running on port ${PORT}`)
