26e0fe0fb8bd94c6af71dd37fa1241e5a98d6687
kanziw <kanziwoong@gmail.com>
2019-09-27 20:20:41 +0900

    Remove and restore deploy.yaml

--- .github/scripts/decrypt_secret.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# https://help.github.com/en/articles/virtual-environments-for-github-actions#limits-for-secrets
-
-# Decrypt the file
-mkdir $HOME/secrets
-# --batch to prevent interactive command --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" \
-  --output $HOME/secrets/$1 .github/scripts/$1.gpg
