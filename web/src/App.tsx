dcf131a1839e9da2d510528e05677d1960e84296
kanziw <kanziwoong@gmail.com>
2019-09-28 18:56:15 +0900

    Add antd and layout

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,21 @@
+import { Layout } from 'antd'
 import React from 'react'
 import { BrowserRouter, Route } from 'react-router-dom'
 import { Home, Slicer } from 'Routes'
 
+const { Header, Content, Footer } = Layout
+
 const App = () => (
-  <BrowserRouter>
-    <Route exact path="/" component={Home} />
-    <Route exact path="/slicer" component={Slicer} />
-  </BrowserRouter>
+  <Layout>
+    <Header>Header</Header>
+    <Content>
+      <BrowserRouter>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/slicer" component={Slicer} />
+      </BrowserRouter>
+    </Content>
+    <Footer>Footer</Footer>
+  </Layout>
 )
 
 export default App
